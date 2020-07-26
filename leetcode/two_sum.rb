95a02dbd55665a1de4e1f577ab1f070d64f2de6e
Shankar T <shankar@tagalys.com>
2020-07-26 18:27:32 +0530

    misc

--- leetcode/two_sum.rb
+++ leetcode/two_sum.rb
@@ -1,5 +1,5 @@
-nums = [2,7,11,15]
-target = 9
+# Runtime: 32 ms, faster than 95.31% of Ruby online submissions for Two Sum.
+# Memory Usage: 10.3 MB, less than 5.68% of Ruby online submissions for Two Sum.
 def two_sum(nums, target)
   indexes = {}
   nums.each_with_index do |num, i|
@@ -9,4 +9,6 @@ def two_sum(nums, target)
   end
 end
 
+nums = [2,7,11,15]
+target = 9
 puts two_sum(nums, target).to_s
